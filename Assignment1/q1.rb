13846c9cc00845577fe02776bdc7e6369190ee0e
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-17 17:58:02 +0530

    Update Answer Question 1

--- Assignment1/q1.rb
+++ Assignment1/q1.rb
@@ -1,21 +1,19 @@
 puts "Enter number"
 
 number = gets.to_i
-flag = false
+flag = 0
 if number > 1
   for i in 2 ... number do
 
    if number % i == 0
-    flag = true
-    puts "Number is not prime"
+    flag = 1
     break
 
    end
   end
 
-  if flag == false
-    puts "Number is prime "
- # else
-  #puts "Number is not prime"
-end
+puts flag == 0? "number is prime":"Number is not prime"
+
+else
+  puts "Enter number greater than 1"
 end
