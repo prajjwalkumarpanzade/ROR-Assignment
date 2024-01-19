dd99e70be99b1dd57e046bfa32908d0df2a75ec1
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-19 16:42:43 +0530

    Update q5

--- Assignment1/ip.rb
+++ Assignment1/ip.rb
@@ -4,18 +4,14 @@ split = IP_add.split('.',0)    #Split input on .
 cls = split[0].to_i            #convert 0 index of splited array to integer
 if (1..127) === cls            #Checking between classes 
   puts "Class A"
-
 elsif (128..191) === cls
   puts "Class B"
-
 elsif (192..223) === cls
   puts "Class C"
 elsif (224..239) === cls
   puts "Class D"
-
 elsif (240..254) === cls
-  puts "Class E"
-  
+  puts "Class E"  
 end
 
 
