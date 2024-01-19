cda099354d15f4e0077ab688bc80fd69a7508f1f
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-19 16:40:03 +0530

    Update q1

--- Assignment1/q1.rb
+++ Assignment1/q1.rb
@@ -1,19 +1,14 @@
 puts "Enter number"
-
 number = gets.to_i
-flag = 0
 if number > 1
   for i in 2 ... number do
-
-   if number % i == 0
-    flag = 1
-    break
-
-   end
+    if number % i == 0
+      puts "Number is not prime"
+      break
+    else 
+      puts "Number is prime"    
+    end
   end
-
-puts flag == 0? "number is prime":"Number is not prime"
-
 else
   puts "Enter number greater than 1"
 end
