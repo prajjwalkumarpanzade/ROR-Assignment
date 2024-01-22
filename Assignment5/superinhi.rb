3e05cef9386af64a912549a704ba78f27428abed
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-01-22 18:06:46 +0530

    Inheritance using super

---
+++ Assignment5/superinhi.rb
@@ -0,0 +1,19 @@
+class Animal
+  def initialize(no_of_legs)
+    @no_of_legs = no_of_legs
+    puts "I am an animal"
+  end
+end
+
+class Cat < Animal
+  def initialize(make_noise,no_of_legs)
+    @make_noise = make_noise
+    super(no_of_legs)
+    puts "I am a cat"
+    puts "I make a noise #{@make_noise}"
+  end
+end
+
+animal = Animal.new(4)
+cat = Cat.new("meow",4)
+
