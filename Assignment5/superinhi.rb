71b3b86955168d4c25dcadb263854544becd28db
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-22 18:11:39 +0530

    Practice inheritance using super keyword

--- Assignment5/superinhi.rb
+++ Assignment5/superinhi.rb
@@ -1,10 +1,10 @@
+#Class creation
 class Animal
   def initialize(no_of_legs)
     @no_of_legs = no_of_legs
     puts "I am an animal"
   end
 end
-
 class Cat < Animal
   def initialize(make_noise,no_of_legs)
     @make_noise = make_noise
@@ -13,7 +13,7 @@ class Cat < Animal
     puts "I make a noise #{@make_noise}"
   end
 end
-
+#Object Creation
 animal = Animal.new(4)
 cat = Cat.new("meow",4)
 
