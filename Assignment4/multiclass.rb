50f77f4cca7d05d4e2757d50a698b05369fb23b3
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-22 18:13:42 +0530

    Practice multiclass creation

--- Assignment4/multiclass.rb
+++ Assignment4/multiclass.rb
@@ -1,3 +1,4 @@
+#Class creation
 class Employee 
   def initialize(id,name,emailid,phone,salary)
     @id = id
@@ -12,24 +13,21 @@ class Employee
     puts "Employee Email ID : #{@email}"
     puts "Employee Phone : #{@phone}"
   end
-
   def display_salary
     puts "Salary : #{@salary}"
   end
-
   def increment_salary_by_amount(amount)
     @salary += amount
     puts "Salary : #{@salary}"
   end
-
   def increment_salary_by_percentage(percentage)
     @salary += @salary * (percentage/100.0)
     puts "Salary : #{@salary}"
   end
 end
-
+#Object Creation
 emp1 = Employee.new(1,"Raj","raj@gmail.com",9112422666,10000)
 emp1.display_details
 emp1.display_salary
 emp1.increment_salary_by_amount(5000)
-# emp1.increment_salary_by_percentage(10)
+emp1.increment_salary_by_percentage(10)
