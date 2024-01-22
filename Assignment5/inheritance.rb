2875bcfc0d7bc05dcfe832ae84dcaa7e7827edf2
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-22 18:10:28 +0530

    Practice Inheritance

--- Assignment5/inheritance.rb
+++ Assignment5/inheritance.rb
@@ -1,26 +1,23 @@
+#Classes creation
 class Animal
   def make_noise
     puts "I can make noise"
   end
 end
-
 class Dog < Animal
   def make_noise
     puts "woof woof"
   end
 end
-
 class Cat < Animal
   def make_noise
     puts "meow meow"
   end
 end
-
+#Object Creation
 animal = Animal.new
 animal.make_noise
-
 moti = Dog.new
 moti.make_noise
-
 mimi = Cat.new
 mimi.make_noise
