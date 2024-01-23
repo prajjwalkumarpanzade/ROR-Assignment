2a18a78a6a89332b38a925fe9178c683831b0417
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-23 17:50:57 +0530

    Update spellings

--- mid-assessment/q9.rb
+++ mid-assessment/q9.rb
@@ -1,20 +1,20 @@
 
 # Create a program to count the number of vowels and consonants in a given string.
-def vovels_count(input_string)
-  vovels = 'aeiouAEIOU'
-  consonents = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+def vowels_count(input_string)
+  vowels= 'aeiouAEIOU'
+  consonants  = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
   v_count = 0
   c_count = 0
   input_string.each_char do |char|
-    if vovels.include?(char)
+    if vowels.include?(char)
       v_count += 1
-    elsif consonents.include?(char)
+    elsif consonants.include?(char)
       c_count += 1
     end
   end
-  puts "Vovels count is #{v_count}"
-  puts "Consonents count is #{c_count}"
+  puts "vowels count is #{v_count}"
+  puts "consonants  count is #{c_count}"
 end
 puts 'Enter the string'
 input_string = gets.chomp
-vovels_count(input_string)
+vowels_count(input_string)
