abf56338e6bf65f85f466187f030845c35188d35
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-01-23 17:47:26 +0530

    Update corner case

--- mid-assessment/q7.rb
+++ mid-assessment/q7.rb
@@ -2,13 +2,18 @@
 def pallindrome?
   print 'Enter a string: '
   given_string = gets.chomp
-  len = given_string.length
-  reverse_string = ''
-  i = len - 1
-  while i >= 0
-    reverse_string << given_string[i]
-    i -= 1
+  if given_string.empty? 
+    puts 'Enter valid string'
+    exit
+  else 
+    len = given_string.length
+    reverse_string = ''
+    i = len - 1
+    while i >= 0
+      reverse_string << given_string[i]
+      i -= 1
+    end
+    given_string == reverse_string
   end
-  given_string == reverse_string
 end
 pallindrome? ? (puts "Given string is pallindrome") : (puts "Given string is not pallindrome")
