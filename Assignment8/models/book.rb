a0c4022e4d08d1fc040229f18de71dbaf140c62b
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-01-30 17:39:35 +0530

    Book Model

---
+++ Assignment8/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ApplicationRecord
+  validates :title, presence: true , uniqueness: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :quantity, format: { with: /\A[0-9]+\z/, message: "Integer only." }
+  has_many :transactions
+end
