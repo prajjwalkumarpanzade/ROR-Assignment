ed55255f632f7de906d74b0f4f6164794910083f
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-05 10:29:34 +0530

    Change

--- Assignment8/models/book.rb
+++ Assignment8/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  validates :title, presence: true , uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :author, presence: true
   validates :genre, presence: true
   validates :quantity, format: { with: /\A[0-9]+\z/, message: "Integer only." }
