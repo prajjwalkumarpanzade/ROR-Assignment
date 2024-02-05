e0f479c95dafbac44e7bf0e341e97fae47ea8182
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-05 10:22:10 +0530

    Add space

--- Assignment5/inheritance.rb
+++ Assignment5/inheritance.rb
@@ -1,20 +1,25 @@
 #Classes creation
-class Animal
+
+class Animal 
   def make_noise
     puts "I can make noise"
   end
 end
+
 class Dog < Animal
   def make_noise
     puts "woof woof"
   end
 end
+
 class Cat < Animal
   def make_noise
     puts "meow meow"
   end
 end
+
 #Object Creation
+
 animal = Animal.new
 animal.make_noise
 moti = Dog.new
