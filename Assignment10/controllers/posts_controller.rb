a17c9d8e29df768206fdf5da377e6ac3f2ba851f
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-05 11:40:12 +0530

    Add line spaces

--- Assignment10/controllers/posts_controller.rb
+++ Assignment10/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :search]
+  
   def index
     @posts = Post.all
   end
+  
   def new 
     @post = Post.new
   end
+  
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -14,12 +17,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  
   def show
     set_post
   end
+  
   def edit
     set_post
   end
+  
   def update
     if @post.update(post_params)
       redirect_to action: :index
@@ -27,15 +33,18 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+  
   def destroy
     set_post
     @post.destroy
     redirect_to action: :index
   end
+  
   private
   def set_post
     @post = Post.find(params[:id])
   end
+  
   def post_params
     params.require(:post).permit(:title,:body,:user_id)
   end
