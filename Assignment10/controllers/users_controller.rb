c30866b109ca407fe8e08db4aa1c0f1b046ee958
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-05 11:41:21 +0530

    Add line spaces

--- Assignment10/controllers/users_controller.rb
+++ Assignment10/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def index 
     @users = User.all  
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save 
@@ -14,11 +17,14 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
   def show
     @user = User.find(params[:id])
   end
   def edit
+    
   end
+  
   def update
     if @user.update(user_params)
       redirect_to action: :index
@@ -26,15 +32,18 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     set_user
     @user.destroy
     redirect_to action: :index
   end
+  
   private
   def set_user
     @user = User.find(params[:id])
   end
+  
   def user_params
     params.require(:user).permit(:name,:email,:age)
   end
