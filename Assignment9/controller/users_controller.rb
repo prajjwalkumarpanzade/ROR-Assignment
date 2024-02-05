b71047175f2f9cbb6d8928a24bcd8d00deac3cb6
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-05 11:29:01 +0530

    Add private methods

--- Assignment9/controller/users_controller.rb
+++ Assignment9/controller/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
     render json: @users
   end
   def create
-    @users = User.new(params.require(:user).permit(:name, :address, :phone_no, :role))
+    @users = user_params
+    @users = User.new(@users)
     if @users.save
       render json: @users, status: :created, location: @users
     end
   end
   def update
-    @user = User.find(params[:id])
+    @user = find_user
     if @user.update(params.require(:user).permit(:name, :address, :phone_no, :role))
       render json: { message: 'User successfully updated.' }, status: 200
     else
@@ -19,18 +20,26 @@ class UsersController < ApplicationController
   end
   def show
     begin
-      @user = User.find(params[:id])
+      @user = find_user
       render json: @user, status: 200
     rescue ActiveRecord::RecordNotFound
       render json: { message: 'User not found' }, status: 404
     end 
   end
   def destroy
-    @user = User.find(params[:id])
+    @user = find_user
     if @user.destroy
       render json: { message: 'User successfully deleted.' }, status: 200
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :address, :phone_no, :role)
+  end
+  def find_user
+    User.find(params[:id])
   end
 end
