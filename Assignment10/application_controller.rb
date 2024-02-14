5116db3f85dfc13e244426d41bdcec97c3e2f0e9
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-14 14:39:45 +0530

    Add Indentation properly

--- Assignment10/application_controller.rb
+++ Assignment10/application_controller.rb
@@ -1,18 +1,16 @@
 class ApplicationController < ActionController::API
-  # protect_from_forgery
   def encode_token(payload)
     JWT.encode(payload, Rails.application.credentials[:secret_key_base]) 
   end
-
   def decoded_token
     header = request.headers['Authorization']
     if header
-        token = header.split(" ")[1]
-        begin
-            JWT.decode(token, Rails.application.credentials[:secret_key_base])
-        rescue JWT::DecodeError
-            nil
-        end
+      token = header.split(" ")[1]
+      begin
+        JWT.decode(token, Rails.application.credentials[:secret_key_base])
+      rescue JWT::DecodeError
+        nil
+      end
     end
   end
 end
