bf014d3a553daca256ad76cd183c132a1fc08fe9
Prajjwalkumar Panzade <65601093+prajjwalkumarpanzade@users.noreply.github.com>
2024-02-19 14:50:40 +0530

    Line space add

--- Assignment12/spec/requests/books_spec.rb
+++ Assignment12/spec/requests/books_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe BooksController, type: :controller do
   describe "GET #index" do
     it "returns a success response" do
       get :index
+      
       expect(response).to be_successful
     end
   end
@@ -13,11 +14,13 @@ RSpec.describe BooksController, type: :controller do
       it "creates a new book" do
         expect {
           post :create, params: { book: { title: "Sample Book", author: "Sample Author", genre: "Sample Genre", quantity: 10 } }
+          
         }.to change(Book, :count).by(1)
       end
 
       it "renders a JSON response with the new book" do
         post :create, params: { book: { title: "Sample Book", author: "Sample Author", genre: "Sample Genre", quantity: 10 } }
+        
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json; charset=utf-8')
         expect(response.location).to eq(book_url(Book.last))
@@ -31,12 +34,14 @@ RSpec.describe BooksController, type: :controller do
     context "with valid params" do
       it "updates the requested book" do
         put :update, params: { id: book.id, book: { title: "Updated Title" } }
+        
         book.reload
         expect(book.title).to eq("Updated Title")
       end
 
       it "renders a JSON response with the book" do
         put :update, params: { id: book.id, book: { title: "Updated Title" } }
+        
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq('application/json; charset=utf-8')
         expect(response.body).to include('Book successfully updated.')
@@ -49,6 +54,7 @@ RSpec.describe BooksController, type: :controller do
 
     it "returns a success response" do
       get :show, params: { id: book.id }
+      
       expect(response).to be_successful
     end
   end
@@ -59,11 +65,13 @@ RSpec.describe BooksController, type: :controller do
     it "destroys the requested book" do
       expect {
         delete :destroy, params: { id: book.id }
+        
       }.to change(Book, :count).by(-1)
     end
 
     it "renders a JSON response with a success message" do
       delete :destroy, params: { id: book.id }
+      
       expect(response).to have_http_status(:ok)
       expect(response.content_type).to eq('application/json; charset=utf-8')
       expect(response.body).to include('Book successfully deleted.')
